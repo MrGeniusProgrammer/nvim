2e8e88f2c4160bd8a701279593e6ac9381ebd44c
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-09-21 13:40:34 +0300

    added harpoon plugin

---
+++ lua/configs/harpoon.lua
@@ -0,0 +1,10 @@
+local options = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+    },
+}
+
+return options
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,6 +31,43 @@ return {
 		end,
 	},
 
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		opts = function ()
+			return require 'configs.harpoon'
+		end,
+		keys = function()
+			local keys = {
+				{
+					"<leader>H",
+					function()
+						require("harpoon"):list():add()
+					end,
+					desc = "Harpoon File",
+				},
+				{
+					"<leader>h",
+					function()
+						local harpoon = require("harpoon")
+						harpoon.ui:toggle_quick_menu(harpoon:list())
+					end,
+					desc = "Harpoon Quick Menu",
+				},
+			}
+
+			for i = 1, 5 do
+				table.insert(keys, {
+					"<leader>" .. i,
+					function()
+						require("harpoon"):list():select(i)
+					end,
+					desc = "Harpoon to File " .. i,
+				})
+			end
+			return keys
+		end,
+	},
 
 	{
 		"lukas-reineke/indent-blankline.nvim",
